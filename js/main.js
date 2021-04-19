9f30cffb88ea8efe3d7c897648aadb0f7e6749e1
Vlad Dedikov <69351396+crew-dev@users.noreply.github.com>
2021-04-19 19:52:36 +0300

    upd//

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-  AOS.init({
-    disable: function() {
-      var maxWidth = 768;
-      return window.innerWidth < maxWidth;
-    }
-  });
-  var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.projects-pagination',
         bulletClass: 'projects-bullet',
